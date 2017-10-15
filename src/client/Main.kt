9b32968d071ab434bb9a07d61d476930b9307411
Andrew Lind <Andrew.Lind@wexinc.co.nz>
2017-10-16 08:00:10 +1300

    added extension function to convert Kotlin map to JS object

--- src/client/Main.kt
+++ src/client/Main.kt
@@ -2,10 +2,17 @@ package client
 
 import client.controllers.*
 import client.services.*
+
 import kotlin.js.*
 
 @JsName("angular")
 external val angular: dynamic
+
+fun HashMap<String, Any?>.toJson(): Json {
+ val js = json()
+ this.forEach { (k, v) -> js.set(k, v) }
+ return js
+}
 
 fun main(args: Array<String>) {
  val controllers = angular.module("controllers", js("[]"))
@@ -17,11 +24,9 @@ fun main(args: Array<String>) {
  val app = angular.module("app", arrayOf("ngRoute", "services", "controllers"))
 
  val config: Array<Any> = arrayOf("\$routeProvider", { routeProvider: dynamic ->
-  //val hello = mapOf("templateUrl" to "/views/hello", "controller" to "HelloController")
-  //val redirect = mapOf("redirectTo" to "/") why doesn't mapOf create a JavaScript object, arrayOf creates a JavaScript array...
-  val hello = json(Pair("templateUrl", "/views/hello"), Pair("controller", "HelloController"))
-  val redirect = json(Pair("redirectTo", "/"))
-  routeProvider["when"]("/", hello).otherwise(redirect)
+  val hello: HashMap<String, Any?> = hashMapOf("templateUrl" to "/views/hello", "controller" to "HelloController")
+  val redirect: HashMap<String, Any?> = hashMapOf("redirectTo" to "/")
+  routeProvider["when"]("/", hello.toJson()).otherwise(redirect.toJson())
  })
 
  app.config(config)
